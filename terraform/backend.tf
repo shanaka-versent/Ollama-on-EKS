468fc578245c7c14f9172a66f741e3c1a3232c5b
shanaka-versent <shanakaj@gmail.com>
2026-03-21 22:52:23 +1100

    docs: update NetworkPolicy and backend config for Stack A deployment
    - Update security table: NetworkPolicy allows ingress from both
      istio-system and istio-ingress namespaces on port 11434
    - Set actual account ID in S3 backend bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -35,7 +35,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "ollama-eks-tfstate-<ACCOUNT_ID>"
+    bucket         = "ollama-eks-tfstate-695418593935"
     key            = "ollama-eks/terraform.tfstate"
     region         = "ap-southeast-2"
     encrypt        = true
